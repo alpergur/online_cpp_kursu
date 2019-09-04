1142c16899698661add72423209c1f11272a8490
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2019-09-04 11:31:40 +0300

    Create c_cpp_diff_2.c

---
+++ c_cpp_diff_2.c
@@ -0,0 +1,10 @@
+// Klasik C döneminden gelen "eski stil fonksiyon tanımlamaları" (old style function definitions) C'de geçerliliğini koruyor.
+// C++ dilinde geçerli değil.
+// Yukarıdaki fonksiyon tanımı C'de geçerli, C++ dilinde geçersiz.
+
+
+sum(a, b, c)
+double a, b, c;
+{
+	return a + b + c;
+}

9b4a20bb880d24b102b93a9345f2f78324959b87
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2019-09-04 11:29:52 +0300

    Update c_cpp_diff_1.c

--- c_cpp_diff_1.c
+++ c_cpp_diff_1.c
@@ -1,9 +1,16 @@
-foo(); //C++'da geçersiz bildirim, C89'da geçerli, C99'da geçersiz bildirim
 
-func()  //C++'da geçersiz bildirim, C89'da geçerli, C99'da geçersiz tanımlama
+// C89'da fonksiyonların "implicit" (örtülü) bildirimi geçerli.
+// C99 standartları ile bu durum geçersiz hale getirildi.
+// Ancak C derleyicilerinin hemen hepsi geçmişe doğru uyumluluğu korumak amacıyla bu duruma izin veriyor.
+
+// C++'da "implicit" (örtülü) işlev bildirimi geçerli değil.
+
+
+void func()
 {
-	const x = 1;  //C++'da geçersiz tanımlama, C89'da geçerli, C99'da geçersiz tanımlama
-	static y = 2; //C++'da geçersiz tanımlama, C89'da geçerli, C99'da geçersiz tanımlama
-
-	return 1;
+	foo();
+	//C89'da geçerli implicit olarak  
+	//int foo();
+	//bildirimi yapılmış kabul ediliyor
+	//C99'da geçersiz C++'da geçersiz
 }
